faeb71a09d92848eee31d62d5ddf862508ea8b64
Jyotishka Datta <jyotishka.datta@gmail.com>
2017-07-08 14:25:12 -0500

    Fixed error in MCMC steps and changed R code

--- R/scale_sigma_hs.R
+++ R/scale_sigma_hs.R
@@ -4,13 +4,26 @@ set.seed(123)
 beta = 20; sigmasq = 1
 X = matrix(c(1,1),byrow=T)
 y = X%*%beta + rnorm(2)
-#library(horseshoe)
+library(horseshoe)
 
 res <- horseshoe(y, X, method.tau = "truncatedCauchy",
                        method.sigma = "Jeffreys",
                  burn = 1000, nmc = 5000, alpha = 0.05)
-#library(ggplot2)
+library(ggplot2)
 samples.hs <- data.frame(values = t(unlist(res$BetaSamples)))
 ggplot(data=samples.hs,aes(x=values))+
   geom_density(position="identity")
 
+setwd("C:/Users/Jyotishka/OneDrive/Documents/R/sqlasso")
+source("eval_sql.R")
+
+Y = y
+res2 <- eval_sql(Y,r = 1, delta = 2, burn = 1000, nmc = 5000)
+samples.sql <- data.frame(values = as.vector(res2$ThetaSave))
+ggplot(data=samples.sql,aes(x=values))+
+  geom_density(position="identity")
+
+plot(res2$ThetaSave[,1])
+
+setwd("C:/Users/Jyotishka/OneDrive/Documents/R/sqlasso")
+dev.copy2pdf(file="theta_bimodal.pdf")
